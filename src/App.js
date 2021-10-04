506bf2ce75e7447942364937b18f8aaf177c3528
Maxim Belomestnov <maximbel@Maxims-MacBook-Air.local>
2021-10-04 12:58:54 -0400

    A very important styling adj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>This is my react site!</h1>
-        <p>Here is some more stuff added to the site!</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>A very user friendly site!</h1>
+      <p>Check it out!</p>
+      <p>
+        Wubba Lubba dub-dubWubba Lubba dub-dubWubba Lubba dub-dubWubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+        Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+        Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+        Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+        Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+        Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+        Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba
+        dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub Wubba Lubba dub-dub
+      </p>
     </div>
   );
 }
